b80ade70bf34287769288d2c57563cc9162c251b
VNatlia <nvolynets371@gmail.com>
2024-10-27 15:43:18 +0200

    done

--- labs/2.3.2.4/main.cpp
+++ labs/2.3.2.4/main.cpp
@@ -98,6 +98,5 @@ int main()
     cin >> orange;
     cout << evaluatePostfix(infixToPostfix(orange));
 
-
     return 0;
 }
---
+++ labs/2.5/main.cpp
@@ -0,0 +1,10 @@
+#pragma once
+struct list
+{
+    int data;
+    list* next;
+};
+inline bool Check (list* element)
+{
+    
+}
